442085099aef24237002f867927f9f7ac321add7
Saheed Adeniji <adeniji4success@gmail.com>
2024-05-21 18:11:32 +0100

    Create Book Entity

--- Bookify.Domain/Shared/Money.cs
+++ Bookify.Domain/Shared/Money.cs
@@ -1,4 +1,6 @@
-﻿namespace Bookify.Domain.Apartments;
+﻿using Bookify.Domain.Apartments;
+
+namespace Bookify.Domain.Shared;
 
 public record Money(decimal Amount, Currency Currency)
 {
@@ -13,4 +15,9 @@ public record Money(decimal Amount, Currency Currency)
     }
 
     public static Money Zero() => new(0, Currency.None);
+
+    public static Money Zero(Currency currency) => new(0, currency);
+
+
+    public bool IsZero() => this == Zero(Currency);
 };
