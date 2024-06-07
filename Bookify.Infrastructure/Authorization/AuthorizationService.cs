635188106448c2f52bb722ee02dcd254b1624b3d
Saheed Adeniji <adeniji4success@gmail.com>
2024-06-07 16:56:04 +0100

    Implement permission based authorization

--- Bookify.Infrastructure/Authorization/AuthorizationService.cs
+++ Bookify.Infrastructure/Authorization/AuthorizationService.cs
@@ -25,4 +25,14 @@ internal sealed class AuthorizationService
 
         return roles;
     }
+
+    public async Task<HashSet<string>> GetPermissionsForUserAsync(string identityId)
+    {
+        var permission = await _context.Set<User>()
+            .Where(user => user.IdentityId == identityId)
+            .SelectMany(user => user.Roles.Select(role => role.Permissions))
+            .FirstAsync();
+
+        return permission.Select(p => p.Name).ToHashSet();
+    }
 }
